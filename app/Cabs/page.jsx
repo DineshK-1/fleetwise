95d0de878abec033c4673b19442ea5421db2a2f0
Mayank Roy <roy050703@gmail.com>
2023-06-16 13:19:19 +0530

    created Cabs page components and some styles

---
+++ app/Cabs/page.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./cabdeets.css";
+
+function CabCard({ reg, name, cabcolor }) {
+  return (
+    <div className="cabcard">
+      <h3> {reg} </h3>
+      <img src="" alt="" />
+      <p>
+        {cabcolor}
+        {name}
+      </p>
+      <div className="icons">
+        <span class="material-icons-outlined">info</span>
+        <span class="material-icons-outlined">more_vert</span>
+      </div>
+    </div>
+  );
+}
+``;
+export default function CabDetails() {
+  const array = [
+    { cabreg: "TN1252165", cabname: "Subaru", cabcolor: "Black" },
+    { cabreg: "TN1252165", cabname: "Subaru", cabcolor: "Black" },
+  ];
+  return (
+    <div>
+      {array.map((cab) => (
+        <CabCard
+          key={cab.cabreg}
+          reg={cab.cabreg}
+          name={cab.cabname}
+          cabcolor={cab.cabcolor}
+        />
+      ))}
+    </div>
+  );
+}
