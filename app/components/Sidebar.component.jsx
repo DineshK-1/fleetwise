7838508b3ef7c168bdaef63bb7b4273d2b79115e
Dinesh <dineshkumar.software@gmail.com>
2023-06-16 13:54:40 +0530

    Revamped Sidebar Positioning

--- app/components/Sidebar.component.jsx
+++ app/components/Sidebar.component.jsx
@@ -8,25 +8,13 @@ export default function Sidebar({ child }) {
     const [expanded, setExpanded] = useState(true);
 
     return (
-        <div className="main-wrapper">
-            <div className="container">
-                <div className="sidebar" style={{ width: expanded ? "175px" : "75px" }}>
-                    {/* Navigation Icons */}
-                    <div className="Navigation">
-                        <NavItem text={"Fleet wise"} icon={"question_mark"} expanded={expanded} link={"/"} />
-                        <NavItem text={"Cabs"} icon={"local_taxi"} expanded={expanded} link={"/Cabs"} />
-                        <NavItem text={"Drivers"} icon={"person"} expanded={expanded} link={"/Drivers"}  />
-                        <NavItem text={"Management"} icon={"manage_accounts"} expanded={expanded} link={"/Mangement"} />
-
-                        <div className="expand" style={{ justifyContent: "center" }} onClick={() => setExpanded((e) => !e)}>
-                            <span className="material-icons-outlined" style={{ transform: expanded ? "rotate(90deg)" : "rotate(270deg)", transition:"all 500ms" }}>expand_more</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="landing-section">
-                {child}
+        <div className="sidebar" style={{ width: expanded ? "175px" : "75px" }} onMouseEnter={() => setExpanded(true)} onMouseLeave={() => setExpanded(false)}>
+            {/* Navigation Icons */}
+            <div className="Navigation">
+                <NavItem text={"Fleet wise"} icon={"question_mark"} expanded={expanded} link={"/"} />
+                <NavItem text={"Cabs"} icon={"local_taxi"} expanded={expanded} link={"/Cabs"} />
+                <NavItem text={"Drivers"} icon={"person"} expanded={expanded} link={"/Drivers"} />
+                <NavItem text={"Management"} icon={"manage_accounts"} expanded={expanded} link={"/Mangement"} />
             </div>
         </div>
     )
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import 'material-icons/iconfont/material-icons.css';
+import 'material-icons/iconfont/outlined.css';
 import Sidebar from './components/Sidebar.component';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Sidebar child={children} />
-        {children}
+        <div className="main-wrapper">
+          {children}
+        </div>
       </body>
     </html>
   )
