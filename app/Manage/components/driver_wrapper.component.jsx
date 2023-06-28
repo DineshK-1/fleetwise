d0b5bb3b8faf99163037c4a98f3907cb82a7cd85
Dinesh <dineshkumar.software@gmail.com>
2023-06-28 15:37:16 +0530

    Working local search functionality

--- app/Manage/components/driver_wrapper.component.jsx
+++ app/Manage/components/driver_wrapper.component.jsx
@@ -5,21 +5,26 @@ import DraggableDriver from "./draggable_driver.component";
 
 export default function DriverWrapper({ Drivers }) {
     const [drivers, setDrivers] = useState(Drivers);
+    const [nameQuery, setNameQuery] = useState("");
+    const [idQuery, setIDQuery] = useState("");
+
 
     return (
         <div className="drivers">
             <h3 className="w-500">Drivers</h3>
             <div className="search-boxes">
-                <input type="text" className="search" placeholder="Search name here" />
-                <input type="text" className="search" placeholder="Search ID here" />
+                <input type="text" value={nameQuery} onChange={(e) => setNameQuery(e.target.value)} className="search" placeholder="Search name here" />
+                <input type="number" value={idQuery} onChange={(e) => setIDQuery(e.target.value)} className="search" placeholder="Search ID here" />
             </div>
 
             {drivers &&
                 drivers.drivers.map((driver) => {
-                    console.log(driver)
-                    return (
-                        <DraggableDriver key={driver.id} first={"test"} second={`${driver.driver_first_name} ${driver.driver_last_name}`} third={driver.driver_ID} Draggable={true} />
-                    )
+                    if ((driver.driver_first_name + " " + driver.driver_last_name).toLowerCase().includes(nameQuery.toLowerCase()) && driver.driver_ID.toString().includes(idQuery)) {
+                        return (
+                            <DraggableDriver key={driver.id} first={"test"} second={`${driver.driver_first_name} ${driver.driver_last_name}`} third={driver.driver_ID} Draggable={true} />
+                        )
+                    }
+
                 })}
         </div>
     )
