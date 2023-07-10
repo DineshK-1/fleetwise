96e1f585c0b989981c757f33cf7c4ba2efd07e09
Dinesh <dineshkumar.software@gmail.com>
2023-07-10 12:39:05 +0530

    Added loading for 2 pages

---
+++ app/Cabs/loading.jsx
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return(
+        <span>Loading</span>
+    )
+}
---
+++ app/drivers/loading.jsx
@@ -0,0 +1,54 @@
+export default function Loading() {
+    return (
+        <div className="driver-section">
+            <h2 className="w-500">Drivers Management</h2>
+            <div className="driver-cards">
+                {[...Array(8)].map((e) => {
+                    return (
+                        <div key={e} className="driver-card">
+                            <div className="card-top">
+                                <div className="left" style={{ width: "100%" }}>
+                                    <div className="image">
+                                        <Image
+                                            src={"/blank-profile.png"}
+                                            alt="driver pic"
+                                            width={64}
+                                            height={64}
+                                            style={{ borderRadius: "5rem" }}
+                                        />
+                                    </div>
+
+                                    <div className="details" style={{ width: "100%" }}>
+                                        <span className="skeleton skeleton-text"></span>
+                                        <span className="skeleton skeleton-text"></span>
+                                    </div>
+                                </div>
+                                <div className="action-buttons">
+                                    <div className="edit-button">
+                                        <span
+                                            className="material-icons-outlined"
+
+                                        >
+                                            edit
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="tags">
+                                <div className="cab tag" style={{ width: "30%" }}><span className="skeleton skeleton-text"></span></div>
+                                <div className="since tag" style={{ width: "25%" }}><span className="skeleton skeleton-text"></span></div>
+                            </div>
+
+                            <div className="tags">
+                                <div className="email tag" style={{ width: "25%" }}><span className="skeleton skeleton-text"></span></div>
+                                <div className="phone tag" style={{ width: "25%" }}><span className="skeleton skeleton-text"></span></div>
+                            </div>
+                        </div >
+                    )
+                })}
+
+            </div>
+        </div>
+    )
+}
