83ee4a92ae320f2e8027738577aa6161999db159
Dinesh <dineshkumar.software@gmail.com>
2023-07-10 15:40:41 +0530

    Color mode defaults to dark

--- app/providers.jsx
+++ app/providers.jsx
@@ -3,5 +3,5 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }) {
-    return <ThemeProvider enableSystem={false}>{children}</ThemeProvider>
+    return <ThemeProvider enableSystem={false} defaultTheme='dark'>{children}</ThemeProvider>
 }
