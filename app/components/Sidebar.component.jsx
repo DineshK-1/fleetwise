8a448a40d9fcd1366d6c800a99964891354bcf47
Dinesh <dineshkumar.software@gmail.com>
2023-07-11 20:37:59 +0530

    Home Page Icon

--- app/components/Sidebar.component.jsx
+++ app/components/Sidebar.component.jsx
@@ -12,7 +12,7 @@ export default function Sidebar() {
         <div className="sidebar" style={{ width: expanded ? "175px" : "75px" }} onMouseEnter={() => setExpanded(true)} onMouseLeave={() => setExpanded(false)}>
             {/* Navigation Icons */}
             <div className="Navigation">
-                <NavItem text={"Fleet wise"} icon={"question_mark"} expanded={expanded} link={"/"} />
+                <NavItem text={"Fleet wise"} icon={"home"} expanded={expanded} link={"/"} />
                 <NavItem text={"Cabs"} icon={"local_taxi"} expanded={expanded} link={"/Cabs"} />
                 <NavItem text={"Drivers"} icon={"person"} expanded={expanded} link={"/drivers"} />
                 <NavItem text={"Management"} icon={"manage_accounts"} expanded={expanded} link={"/Manage"} />
